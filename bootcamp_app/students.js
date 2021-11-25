7a5459eef6772dad652074c855cc5fb9e53a48bb
Neema <patelneema93@gmail.com>
2021-11-25 00:11:57 -0500

    i making teacher and studentselect query

---
+++ bootcamp_app/students.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'heeya',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+const cohort_name = process.argv[2];
+const limit = process.argv[3];
+pool.query(`
+SELECT students.id as student_id, students.name as name, $1 as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+LIMIT $2;
+`,[cohort_name, limit])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
